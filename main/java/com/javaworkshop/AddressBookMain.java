f41802a8c9673519e98f63f75d916b1d436a6647
sanketb412 <sanketbagde38@gmail.com>
2021-06-03 12:55:20 +0530

    Added: code for Creating a Contact for AddressBook

--- main/java/com/javaworkshop/AddressBookMain.java
+++ main/java/com/javaworkshop/AddressBookMain.java
@@ -1,12 +1,24 @@
 package com.javaworkshop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("--Welcome to Address Book Program--");
     }
 
-    public boolean addContact(Contacts contact) {
-        return true;
+    /**
+     * For Testing created Method
+     * @param contactList
+     * @return
+     */
+    public Boolean addContact(Contacts contactList) {
+        List<Contacts> contacts = new ArrayList<>();
+        if(contacts.add(contactList)) {
+            return true;
+        }
+        return false;
     }
 }
--- test/java/addressTest/AddressBookTest.java
+++ test/java/addressTest/AddressBookTest.java
@@ -7,11 +7,15 @@ import org.junit.jupiter.api.Test;
 
 public class AddressBookTest {
 
+    /**
+     * Getting test case for Adding contacts
+     * and returning True
+     */
     @Test
     public void GivenContact_WhenReturn_ShouldPassTest() {
         AddressBookMain addressBook = new AddressBookMain();
         Contacts contact = new Contacts("sanket", "Bagde", "Mulund", "Mumbai", "Maharashtra", 4000080, 8174785, "sanketbdg@fgmil.com");
-        boolean result = addressBook.addContact(contact);
+        Boolean result = addressBook.addContact(contact);
         Assertions.assertTrue(result);
     }
 }
